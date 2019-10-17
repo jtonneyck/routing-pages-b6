879832c4b50c85f02c1717a1a8024d8a036d5805
Piepongwong <j.tonneyck@gmail.com>
2019-10-17 10:54:33 +0200

    simple page routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Link, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Link to="/Home">Home</Link>
+      <Link to="/About">About</Link>
+      <Link to="/Contact">Contact</Link>
+
+      {/* Notation 1 */}
+      {/* <Route path="/home">
+        <Home/>
+      </Route> */}
+
+      <Route path="/home" component={Home} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/about" component={About} />
+
     </div>
   );
 }
